c3b90fcdaac4aee174e25652010c34811d217ad6
wangqr <wangqirui1996@gmail.com>
2015-12-13 16:38:00 +0800

    修了一些bug（我也不确定有哪些……）

--- src/action.py
+++ src/action.py
@@ -2,82 +2,30 @@
 # -*- coding: utf-8 -*-
 
 import json
-import ai_proxy
+import threading
 
-def sendInstruction(string):
-    '''
-    临时替代逻辑组的sendInstruction
-    '''
-    pass
-
-'''
-以下的类声明仅供参考，最终的json化可能在python中实现，也可能直接在c中完成。
-'''
 
 class Action:
-    def __init__(self, action_json, action_name = None):
-        self.ai_id = ai_id
-        if (action_name):
-            self.action_name = action_name
-        else:
-            if (act["action"] in ["init", "move", "use_skill", "upgrade_skill"]):
-                self.action_name = 'instruction'
-            elif (act["action"] in ["query_map", "query_status"]):
-                self.action_name = 'query'
+    def __init__(self, action_json, action_name, return_queue):
+        self.action_name = action_name
         self.action_json = action_json
+        self.return_queue = return_queue
+
     def run(self, logic):
-        if (self.action_name == 'instruction'):
-            logic.setInstruction(self.action_json)
-        elif (self.action_name == 'query'):
-            ret = logic.getInstruction(self.action_json)
-            # TODO send back data
-            #ai_proxy.update_info(ret, self.ai_id)
-    def set_timestamp(time: int):
+        if self.action_name == 'instruction':
+            try:
+                logic.setInstruction(self.action_json)
+            except Exception:
+                pass
+        elif self.action_name == 'query':
+            try:
+                ret = logic.getInstruction(self.action_json)
+            except Exception as e:
+                print('[ERROR] \x1b[1;31mlogic exception '+type(e).__name__+'\x1b[m')
+                ret = '{"message": "logic exception '+type(e).__name__+'"}'
+            self.return_queue.put(ret)
+
+    def set_timestamp(self, time: int):
         q = json.loads(self.action_json)
-        q['time'] = time
+        q['time'] = int(time)
         self.action_json = json.dumps(q)
-
-class QueryMap(Action):
-    def __init__(self, ai_id):
-        Action.__init__(self, ai_id, action_name="query")
-    def run(self):
-        r = sendInstruction(json.dumps(self.__dict__))
-        sendBackToAI(self.ai_id, b'{"mapinfo":' + r + b'}')
-
-class Move(Action):
-    def __init__(self, ai_id, x, y, z):
-        Action.__init__(self, ai_id, action_name="move")
-        self.x=x
-        self.y=y
-        self.z=z
-
-class UseSkill(Action):
-    def __init__(self, ai_id, skill_type, dir_x, dir_y, dir_z, target_obj_id):
-        Action.__init__(self, ai_id, action_name="skill")
-        self.type=skill_type
-        self.x=dir_x
-        self.y=dir_y
-        self.z=dir_z
-        self.target=target_obj_id
-
-class UpdateSkill(Action):
-    def __init__(self, ai_id, skill_type):
-        Action.__init__(self, ai_id, action_name="skill")
-        self.type=skill_type
-        self.x=dir_x
-        self.y=dir_y
-        self.z=dir_z
-        self.target=target_obj_id
-
-class PauseAction(Action):
-    def __init__(self, ai_id):
-        Action.__init__(self, ai_id, action_name="_pause")
-    def run(self):
-        pass # should be handled by platform
-
-class GameEndAction(Action): # generated when time up
-    def __init__(self, forced = False):
-        Action.__init__(self, ai_id = -1, action_name="_end")
-        self.forced=forced
-    def run(self):
-        pass # should be handled by platform
--- src/main.py
+++ src/main.py
@@ -17,93 +17,124 @@ options:
     -t <timelimit> set the time limit of the game in seconds
 '''
 
-import argparse
+
 import docopt
 import queue
-import sys
+import json
+import random
+import os
+import signal
 import time
 import threading
 
 import ai_proxy
 import action
+#   Bug remain
+# import logger
 
 import ts17core
 import ts17core.interface
 
-
-__version__='0.1-a'
+__version__ = '0.1-a'
 
 action_queue = queue.PriorityQueue()
 game_start_time = 0
 game_paused = False
 game_pause_time = 0
+time_limit = 0
+
 
 def push_queue_ai_proxy(obj: str):
     global action_queue
     global game_start_time
     timestamp = time.time() - game_start_time
-    if(game_paused):
+    if game_paused:
         timestamp = game_pause_time - game_start_time
-    action_queue.put((timestamp, action.Action(obj)))
+    act = json.loads(obj).get('action')
+    action_name = None
+    ret = None
+    if act in ["init", "move", "use_skill", "upgrade_skill"]:
+        action_name = 'instruction'
+    elif act in ["query_map", "query_status"]:
+        action_name = 'query'
+        ret = queue.Queue()
+    action_queue.put((timestamp, action.Action(obj, action_name, ret)))
+    ret_str = None
+    if ret:
+        print('['+str(time.time() - game_start_time)+'] waiting for ret_str')
+        ret_str = ret.get(block=True)
+        print('['+str(time.time() - game_start_time)+'] core returned \''+str(ret_str)+'\'')
+    return ret_str
+
 
 def main():
     args = docopt.docopt(__doc__, version = 'ts17-platform ' + __version__ + ' [ts17-core ver ' + ts17core.__version__ + ']')
-    if(args['run']):
+    if args['run']:
         run_main(args)
-    elif(args['replay']):
+    elif args['replay']:
         replay_main(args)
     else:
         docopt.docopt(__doc__, argv=['-h'])
 
+
 class EndSignalGenerator (threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
+
     def run(self):
-        while(1):
+        global action_queue
+        while 1:
             current_time = time.time()
-            if (current_time - game_start_time > time_limit):
-                action_queue.put((0, action.Action(0, None, '_end')))
+            if current_time - game_start_time > time_limit:
+                end_action = action.Action('{"action":"_platform"}', "_end", None)
+                print('['+str(time.time() - game_start_time)+'] put stop sig')
+                action_queue.put((0, end_action))
+                return
             else:
-                time.sleep(game_start_time + time_limit - current_time)
+                time.sleep((game_start_time + time_limit - current_time)*1)
+
 
 def run_main(args: dict):
     global action_queue
     global game_start_time
+    global game_pause_time
+    global time_limit
+    global game_paused
 
     last_action_timestamp = 0
 
     # init logic
     main_logic = ts17core.interface.Interface()
 
-    # init ai_proxy
-    ai_proxy.start(args['<ai>'], push_queue_ai_proxy)
-
-    # init ui
-    # TODO
-
-    # get init action
-    init_json = '{"action":"init","seed":' + str(random.randrange(0,4294967296)) + '}'
-    main_logic.setInstruction(init_json)
-
-    # init logger
-    logger = logger.Run_Logger(init_json)
-
     game_started = True
     game_start_time = time.time()
 
-    time_limit=0
-    if(args['-t']):
-        time_limit=int(args['-t'])
+    # init ai_proxy
+    ai_proxy.start(args['<ai>'], push_queue_ai_proxy)
 
-    if(time_limit > 0):
+    #   init ui
+    # TODO
+
+    #   get init action
+    init_json = '{"action":"init","seed":' + str(random.randrange(0,4294967296)) + '}'
+    main_logic.setInstruction(init_json)
+
+    #   init logger
+    # run_logger = logger.Run_Logger(init_json)
+
+    if args['-t']:
+        time_limit = int(args['-t'])
+
+    if time_limit > 0:
         EndSignalGenerator().start()
 
     # main loop
-    while(game_started):
+    while game_started:
         next_action = action_queue.get(block=True)
-        if (next_action[1].action_name == '_pause'):
-            if (args['-d']):
-                if(game_paused):
+        print('['+str(time.time() - game_start_time)+'] \x1b[1;32m>>>>>>>>sig recv id=' + str(json.loads(next_action[1].action_json).get('ai_id')) +' ' + json.loads(next_action[1].action_json).get('action') + ' '+ next_action[1].action_name +'\x1b[m')
+        if next_action[1].action_name == '_pause':
+            if args['-d']:
+                if game_paused:
                     game_start_time += time.time() - game_pause_time
                     game_paused = False
                 else:
@@ -112,20 +143,26 @@ def run_main(args: dict):
             else:
                 print('illegal action: pause')
             continue
-        elif (next_action[1].action_name == '_end'):
-            break;
-        if (next_action[0] > last_action_timestamp):
+        elif next_action[1].action_name == '_end':
+            print('['+str(time.time() - game_start_time)+'] \x1b[1;33mstop sig detected\x1b[m')
+            break
+        if next_action[0] > last_action_timestamp:
             last_action_timestamp = next_action[0]
-        if(last_action_timestamp > time_limit):
+        if time_limit and last_action_timestamp > time_limit:
             break
         next_action[1].set_timestamp(last_action_timestamp)
-        if (next_action[1].action_name != 'query'):
-            logger.log_action(next_action[1])
+        if next_action[1].action_name != 'query':
+            # run_logger.log_action(next_action[1])
+            pass
         next_action[1].run(main_logic)
-    ai_proxy.stopAI()
+        print('['+str(time.time() - game_start_time)+'] \x1b[1;32m<<<<<<<<fin id=' + str(json.loads(next_action[1].action_json).get('ai_id')) +' ' + json.loads(next_action[1].action_json).get('action') +'\x1b[m')
+    # ai_proxy.stopAI()
+    print('['+str(time.time() - game_start_time)+'] \x1b[1;31mquit\x1b[m')
+    os.kill(os.getpid(), signal.SIGTERM)
+
 
 def replay_main(args: dict):
     pass
 
-if (__name__=='__main__'):
+if __name__=='__main__':
     main()
