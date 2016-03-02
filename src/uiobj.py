bf5feec590049a32954e1d3817ebd88a85950e7b
wangqr <wangqirui1996@gmail.com>
2016-03-02 23:15:17 +0800

    重写uiobj

--- src/uiobj.py
+++ src/uiobj.py
@@ -1,9 +1,82 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import json
+import main
+import action
+import queue
 import socket
+import json
 import threading
+
+
+class RecvThread(threading.Thread):
+    def __init__(self, sock, signal_queue, queue_function, bufsize=4096):
+        threading.Thread.__init__(self)
+        self.socket = sock
+        self.queue_function = queue_function
+        self.bufsize = bufsize
+        self._send_signal_queue = signal_queue
+        self.sig = queue.Queue()
+
+    def run(self):
+        buf = ''
+        while 1:
+            try:
+                data = self.socket.recv(self.bufsize).decode()
+            except UnicodeDecodeError as e:
+                main.root_logger.error('platform (UI) exception %s [%s]', type(e).__name__, str(e))
+                continue
+            except OSError:
+                break
+            if data == '':
+                break
+            if data is not None:
+                buf += data
+                buf = buf[buf.find('{'):]
+                p = buf.find('}')
+                while p >= 0:
+                    self.queue_function(buf[:p+1])
+                    buf = buf[buf.find('{', p+1):]
+                    p = buf.find('}')
+        self._send_signal_queue.put(1)
+
+
+class SendThread(threading.Thread):
+    def __init__(self, sock, signal_queue):
+        threading.Thread.__init__(self)
+        self.socket = sock
+        self._send_signal_queue = signal_queue
+        self.sig = queue.Queue()
+
+    def run(self):
+        while 1:
+            q = self.sig.get(block=True)
+            if q == 0:
+                break
+            else:
+
+                # 强行预处理
+                data_type = ''
+                try:
+                    j = json.loads(q)
+                    if j.get('players') is not None:
+                        data_type = 'query_status'
+                    elif j.get('objects') is not None:
+                        data_type = 'query_map'
+                except ValueError:
+                    pass
+                if data_type != '':
+                    q = load_msg_from_logic(q, data_type)
+                if not q.endswith('\n'):
+                    q += '\n'
+
+                try:
+                    self.socket.send(q.encode())
+                except BrokenPipeError:
+                    break
+                except OSError:
+                    break
+        self._send_signal_queue.put(1)
 
 
 def load_msg_from_logic(msg: str, action_name: str) -> str:  # 从 ai_proxy 移植过来
@@ -38,55 +111,58 @@ def load_msg_from_logic(msg: str, action_name: str) -> str:  # 从 ai_proxy 移�
     return ret_str  # ret_str 的末尾有一个 \n
 
 
-class SocketThread(threading.Thread):
-    def __init__(self, sock, queue_function, bufsize = 4096):
+class UIObject(threading.Thread):
+    def __init__(self, game, host='localhost', port=6000, backlog=1, ai_id=-1):
         threading.Thread.__init__(self)
-        self.socket = sock
-        self.clientsocket = None
-        self.clientaddress = None
-        self.queue_function = queue_function
-        self.bufsize = bufsize
-        self.end = False
-
-    def run(self):
-        while not self.end:
-            try:
-                self.clientsocket, self.clientaddress = self.socket.accept()
-            except OSError:
-                continue
-            if __debug__:
-                print('[INFO] platform (UI) Established connection from ' + str(self.clientaddress))
-            while not self.end:
-                try:
-                    data = self.clientsocket.recv(self.bufsize).decode()
-                except UnicodeDecodeError as e:
-                    print('[ERROR] \x1b[1;31mplatform (UI) exception ' + type(e).__name__ + ' [' + str(e) + ']\x1b[m')
-                    ret = '{"message": "platform (UI) exception ' + type(e).__name__ + ' [' + str(e) + '"]}'
-                    data = None
-                if data == '':
-                    self.clientsocket.close()
-                    if __debug__:
-                        print('[INFO] platform (UI) Connection reset by peer.')
-                    break
-                lines = data.split('\n')
-                for data in lines:  # 这样处理可以吗?
-                    if data is not None:
-                        ret_json = self.queue_function(data)
-                        action_name = json.loads(data)['action']
-                        ret = load_msg_from_logic(ret_json, action_name)  # 使用和选手接口同样的处理方式 by mxj
-                    if ret is not None:
-                        self.clientsocket.send(ret.encode())
-
-
-class UIObject:
-    def __init__(self, queue_function, host = 'localhost', port = 6000, backlog = 1, ai_id = -1):
         self.socket = socket.socket()
         self.socket.bind((host, port))
         self.socket.listen(backlog)
-        self.socket_thread = SocketThread(self.socket, queue_function)
-        self.socket_thread.start()
+        self.sig = queue.Queue()
+        self.recv_thread = None
+        self.send_thread = None
+        self.ui_socket = None
+
+        self._game_obj = game
+        self._ai_id = ai_id
 
     def exit(self):
-        self.socket_thread.end = True
         self.socket.close()
-        self.socket_thread.join()
+        self.sig.put(0)
+        self.__exit_child_threads()
+
+    def __exit_child_threads(self):
+        if self.ui_socket:
+            self.ui_socket.close()
+        if self.recv_thread and self.recv_thread.is_alive():
+            self.recv_thread.sig.put(0)
+        if self.send_thread and self.send_thread.is_alive():
+            self.send_thread.sig.put(0)
+        self.recv_thread.join()
+        self.send_thread.join()
+
+    def push_queue_ui(self, obj: str):
+        json_obj = json.loads(obj)
+        act = json_obj.get('action')
+        timestamp = json_obj.get('time') or self._game_obj.current_time
+        if act and act[0] == '_':
+            self._game_obj.enqueue(0., action.Action('{"action":"_platform","ai_id":%d}' % self._ai_id, act, None))
+        else:
+            self._game_obj.enqueue(timestamp, action.Action(obj, 'query', self.send_thread.sig))
+
+    def enqueue(self, data: str):
+        if self.send_thread and self.send_thread.is_alive():
+            self.send_thread.sig.put(data)
+
+    def run(self):
+        while 1:
+            q = self.sig.get(block=True)
+            if q == 0:
+                break
+            if q == 1:
+                main.root_logger.info('[INFO] platform (UI) Connection reset by peer.')
+                self.__exit_child_threads()
+            self.ui_socket, _ = self.socket.accept()
+            self.recv_thread = RecvThread(self.ui_socket, self.sig, self.push_queue_ui)
+            self.recv_thread.start()
+            self.send_thread = SendThread(self.ui_socket, self.sig)
+            self.send_thread.start()
