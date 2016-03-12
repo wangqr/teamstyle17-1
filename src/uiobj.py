2010c149e1fb802edebe565e1f43d139b8d82bb5
wangqr <wangqirui1996@gmail.com>
2016-03-12 20:13:15 +0800

    add return value

--- src/uiobj.py
+++ src/uiobj.py
@@ -217,7 +217,8 @@ class UIObject(threading.Thread):
         act = json_obj.get('action')
         timestamp = json_obj.get('time') or self._game_obj.current_time
         if act and act[0] == '_':
-            self._game_obj.enqueue(timestamp, action.Action('{"action":"_platform","ai_id":%d}' % self._ai_id, act, None))
+            self._game_obj.enqueue(timestamp, action.Action('{"action":"_platform","ai_id":%d}' % self._ai_id, act,
+                                                            self.send_thread.sig))
         else:
             self._game_obj.enqueue(timestamp, action.Action(obj, 'query', self.send_thread.sig))
 
